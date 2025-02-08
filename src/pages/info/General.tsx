b903047fa30151eddcb794f499f691a09e576e41
MuhammadWaleedMalik <148714412+MuhammadWaleedMalik@users.noreply.github.com>
2025-02-09 01:04:18 +0500

    ad

--- src/pages/info/General.tsx
+++ src/pages/info/General.tsx
@@ -6,37 +6,41 @@ const General: React.FC = () => {
   const user = useSelector((state: RootState) => state.user);
 
   return (
-    <div>
+    <div className="p-4 sm:p-6">
       <h1 className="text-2xl font-bold mb-6">General</h1>
       
       <div className="bg-white rounded-lg shadow p-6">
         <h2 className="text-lg font-semibold mb-4">Registration</h2>
         
         <div className="space-y-4">
-          <div className="grid grid-cols-2 gap-4">
+          {/* Grid for PCN and Student Number */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <div>
               <p className="text-gray-600">PCN</p>
-              <p>{user.pcn}</p>
+              <p className="font-medium">{user.pcn}</p>
             </div>
             <div>
               <p className="text-gray-600">Student number</p>
-              <p>{user.studentNumber}</p>
+              <p className="font-medium">{user.studentNumber}</p>
             </div>
           </div>
           
+          {/* Name */}
           <div>
             <p className="text-gray-600">Name</p>
-            <p>{user.name}</p>
+            <p className="font-medium">{user.name}</p>
           </div>
           
+          {/* First Name */}
           <div>
             <p className="text-gray-600">First name</p>
-            <p>{user.firstName}</p>
+            <p className="font-medium">{user.firstName}</p>
           </div>
           
+          {/* Registration Status */}
           <div>
             <p className="text-gray-600">Registration status</p>
-            <p>{user.registrationStatus}</p>
+            <p className="font-medium">{user.registrationStatus}</p>
           </div>
         </div>
       </div>
