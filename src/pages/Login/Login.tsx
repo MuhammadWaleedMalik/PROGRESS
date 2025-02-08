be706e61175f74ab6f559c6e13bec9f234f1366e
MuhammadWaleedMalik <148714412+MuhammadWaleedMalik@users.noreply.github.com>
2025-02-09 00:49:14 +0500

    remove bugs

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
+
 interface LoginProps {
   onLogin: (isLoggedIn: boolean) => void;
 }
@@ -11,8 +12,6 @@ function Login({ onLogin }: LoginProps) {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  
-
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -20,7 +19,6 @@ function Login({ onLogin }: LoginProps) {
     if (email === "i47732@student.fontys.nl" && password === "igT7wtrM") {
       onLogin(true);
       navigate("/dashboard");
-      
     } else {
       setError("Invalid email or password");
     }
@@ -32,9 +30,9 @@ function Login({ onLogin }: LoginProps) {
       <div className="image-section"></div>
 
       {/* Right Side: Login Form */}
-      <div className="login-box w-[480px]">
-        <div className="logo mr-[200px] mb-12"></div>
-        <h2 className="mr-[120px] mt-4 mb-4 w-[280px]">Sign in with your Fontys account.</h2>
+      <div className="login-box">
+        <div className="logo"></div>
+        <h2 className="mr-[120px] mt-10 ">Sign in with your Fontys account.</h2>
         <form onSubmit={handleSubmit}>
           <input
             type="email"
@@ -52,19 +50,19 @@ function Login({ onLogin }: LoginProps) {
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-          {error && <p className="text-red-500">{error}</p>}
-          <button type="submit" className="sign-in-button w-28 h-10 text-center mr-[290px] mt-10">
+          {error && <p className="error-message">{error}</p>}
+          <button type="submit" className="sign-in-button">
             Sign in
           </button>
         </form>
-        <p className="mt-5 mr-[80px] w-80">
+        <p className="footer-text mt-14">
           Signing in requires a{" "}
           <a href="#">
             <span className="text-blue-400">Fontys account</span>
           </a>
           .
         </p>
-        <div className="footer-links mt-20 mr-[80px] w-80">
+        <div className="footer-links">
           <a href="#" className="footer-link">
             Privacy
           </a>
--- src/store/slices/uiSlice.ts
+++ src/store/slices/uiSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  isSidebarOpen: false,
+  isSidebarOpen: true,
 };
 
 const uiSlice = createSlice({
